ecb89dac6a1c393c9cd2346cbebb051483179ae5
emiel-1094988 <1094988@hr.nl>
2025-02-01 16:28:39 +0100

    404 page routing
    When a page is not found, simply show a 404 page

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Default Route
 @app.route("/")
 def homepagina():
     return render_template("homepagina.html")
 
+@app.errorhandler(404)
+def notFound(e):
+    return render_template("pagina-niet-gevonden.html")
+
+# Route setup for onderzoeksvragen page
 @app.route("/onderzoeksvragen")
 def onderzoeksvragen():
     return render_template("onderzoeksvragen.html")
