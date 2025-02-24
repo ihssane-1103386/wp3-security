e74f70c90145200c2015e512cca83b2e8a8cbe84
ihssane_1103386 <1103386@hr.nl>
2025-02-24 23:28:40 +0100

    added a toggle button to change the theme to dark mode.

--- static/js/theme.js
+++ static/js/theme.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const savedTheme = localStorage.getItem("theme") || "light";
     setTheme(savedTheme);
 
+    const toggleButton = document.createElement()
+    toggleButton.innerText = savedTheme === "dark" ? "Light Mode" : "Dark Mode";
+    toggleButton.id = "themeToggle";
+    document.body.appendChild(toggleButton);
 })
