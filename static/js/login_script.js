d7cd02640b10b10c8fd057b4e30327158c53c79b
Anmol Haribhajan <1096493@hr.nl>
2025-02-26 01:54:32 +0100

    login added

---
+++ static/js/login_script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("#login-page");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+
+        if (!email || !password) {
+            alert("Vul alle velden in.");
+            return;
+        }
+
+        fetch("/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email: email, password: password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                localStorage.setItem("token", data.token); // Opslaan van token
+                window.location.href = "/onderzoeksvragen";
+            } else {
+                alert(data.message);
+            }
+            })
+        .catch(error => {
+            console.error("Login error:", error);
+            alert("Er is een probleem met de server. Probeer het later opnieuw.");
+        });
+    });
+});
