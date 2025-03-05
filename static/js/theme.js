1b9f5c763e0e4c7654417860427be89ac375ce23
ihssane_1103386 <1103386@hr.nl>
2025-03-05 15:31:57 +0100

    cookie set

--- static/js/theme.js
+++ static/js/theme.js
@@ -25,6 +25,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function setCookie(name, value, days) {
     const d = new Date();
     d.setTime(d.getTime() + (days*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
 function setTheme(mode) {
