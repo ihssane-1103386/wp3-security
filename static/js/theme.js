fa4677247d2e07a8ac856562c8d191cca6808aa5
ihssane_1103386 <1103386@hr.nl>
2025-03-05 23:58:51 +0100

    for loop to go through each cookie in ca

--- static/js/theme.js
+++ static/js/theme.js
@@ -31,7 +31,9 @@ function setCookie(name, value, days) {
 function getCookie(name) {
     const nameEQ = name + "=";
     const ca = document.cookie.split(';');
-
+    for (let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+    }
 
 }
 
