f9ed1211c6d368780ba659b89882e2729e32b73a
Artur(1096202) <1096202@hr.nl>
2025-03-08 14:05:11 +0100

    A new app route has been added for updating research questions.

--- app.py
+++ app.py
@@ -166,6 +166,23 @@ def updateRegistrationStatus(table_name):
 def getOnderzoeken():
     return onderzoeken.getOnderzoeken()
 
+
+@app.route("/api/update-onderzoeksvraag", methods=["PATCH"])
+def update_onderzoeksvraag():
+    data = request.json
+    onderzoek_id = data.get("onderzoek_id")
+
+    if not onderzoek_id:
+        return jsonify({"error": "onderzoek_id is vereist"}), 400
+
+    update_result = Onderzoeksvragen.update_onderzoeksvraag(onderzoek_id, data)
+
+    if update_result:
+        return jsonify({"message": "Onderzoeksvraag succesvol bijgewerkt"}), 200
+    else:
+        return jsonify({"error": "Fout bij updaten van onderzoeksvraag"}), 500
+
+
 @app.route("/api/onderzoeken/inschrijvingen/<int:id>", methods=["GET"])
 def getInschrijvingen(id):
     return Inschrijvingen.getInschrijvingen(id)
