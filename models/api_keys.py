8b013c09829be340515163bbf3964abbf16e8f99
Artur(1096202) <1096202@hr.nl>
2025-03-09 12:53:41 +0100

    Made new class ApiKeys and staticmethod to get apikeys

--- models/api_keys.py
+++ models/api_keys.py
@@ -1,3 +1,12 @@
 import secrets
 from models.database_connect import RawDatabase
 
+class ApiKeys:
+    @staticmethod
+    def get_by_key(api_key):
+        query = """
+            SELECT * FROM api_keys
+            WHERE api_key = ?
+        """
+        result = RawDatabase.runRawQuery(query, (api_key,))
+        return dict(result[0]) if result else None
