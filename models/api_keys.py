32991f20341747ee02f2a620588ea3a768e54913
Artur(1096202) <1096202@hr.nl>
2025-03-13 17:47:27 +0100

    Made a function to create missing api keys

--- models/api_keys.py
+++ models/api_keys.py
@@ -3,33 +3,19 @@ from models.database_connect import RawDatabase
 
 class ApiKeys:
     @staticmethod
-    def get_by_onderzoek_id(onderzoek_id):
-        query = """
-                SELECT * FROM api_keys
-                WHERE onderzoek_id = ?
-            """
-        result = RawDatabase.runRawQuery(query, (onderzoek_id,))
-        return dict(result[0]) if result else None
-
-    @staticmethod
-    def get_by_key(api_key):
-        query = """
-            SELECT * FROM api_keys
-            WHERE api_key = ?
-        """
-        result = RawDatabase.runRawQuery(query, (api_key,))
-        return dict(result[0]) if result else None
-
-    @staticmethod
-    def create_key(organisatie_id, onderzoek_id):
+    def create_key(organisatie_id):
         try:
             key = secrets.token_urlsafe(32)
             print(f"Generated API key: {key}")  # Debug print
+
+            # Voeg de API-sleutel toe aan de 'organisaties' tabel
             query = """
-                           INSERT INTO api_keys (organisatie_id, onderzoek_id, api_key)
-                           VALUES (?, ?, ?)
-                       """
-            RawDatabase.runInsertQuery(query, (organisatie_id, onderzoek_id, key))
+                UPDATE organisaties 
+                SET api_key = ?
+                WHERE organisatie_id = ?
+            """
+            RawDatabase.runInsertQuery(query, (key, organisatie_id))
+
             return key
         except Exception as e:
             print(f"Error creating API key: {e}")  # Foutmelding als er iets misgaat
